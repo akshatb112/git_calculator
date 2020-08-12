5cf7fac77f7a85e32cfb565937d1190e6a5eac65
akshatb112 <akshat.bhandari112@gmail.com>
2020-08-12 12:30:01 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+print("CALCULATOR")
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Power")
+
+choice = input("Enter choice(1/2/3/4/5): ")
+
+if choice in ('1', '2', '3', '4','5'):
+        #add
+
+
+        #subtract
+
+
+        #multiply
+
+
+        #divide
+
+
+        #power
+else: 
+    print("Invalid input")
